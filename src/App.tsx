d452e7c618b21882881fb29219255f3921479010
Amit Mishra <amit.3.mishra@eviden.com>
2024-05-30 18:12:44 +0530

    dependency conflict update

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { LiquidityPlanner } from "./LiquidityPlanner";
 
 const App: React.FC = () => {
-  // Define header colors for each column
+  const [colorName, setColorName] = useState('blue');
+
+  const handleColorChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setColorName(event.target.value);
+  };
 
   return (
     <div className="liquidity-planner-app">
@@ -13,11 +17,16 @@ const App: React.FC = () => {
           justifyContent: "start",
         }}
       >
-        <h1 style={{ marginBottom: "0px" }}>Financial liquidity planner By Amit Mishra</h1>
+        <h1 style={{ marginBottom: "0px" }}>Financial Liquidity Planner By Amit Mishra</h1>
+        <input
+          type="text"
+          placeholder="Please enter color name"
+          onChange={handleColorChange}
+        />
       </div>
 
       {/* Pass headerColors as prop to LiquidityPlanner */}
-      <LiquidityPlanner headerColors='blue' />
+      <LiquidityPlanner headerColors={colorName} />
     </div>
   );
 };
